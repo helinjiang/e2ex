578b885cc7440a0939bab3bd33345c6be5c1536e
helinjiang <me@helinjiang.com>
2018-04-19 20:33:13 +0800

    feat: 更新 builder

--- demo/e2ex.config.js
+++ demo/e2ex.config.js
@@ -1,3 +1,9 @@
+const path = require('path');
+
 module.exports = {
-    name: 'e2ex'
+    name: 'e2ex',
+    srcPath: path.join(__dirname, 'src-client-script'),
+    distPath: path.join(__dirname, 'dist-client-script'),
+    entryRelativePath: './output',
+    minifyJS: true
 };
