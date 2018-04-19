3f36fe30b394290e75e6387cafead3bfe5653c85
helinjiang <me@helinjiang.com>
2018-04-19 22:31:01 +0800

    feat: 增加参数支持 -s 代表简单模式

--- demo/src-client-script/output/comic/get-page-info.js
+++ demo/src-client-script/output/comic/get-page-info.js
@@ -2,7 +2,10 @@
 // eval(require('raw-loader!../../modules/nightmare-preload.js'));
 // == inline end ==
 
-const utils = require('../../modules/utils');
+// const utils = require('../../modules/utils');
+import utils from '../../modules/utils';
+
+console.log('xxxxxxxxxxx', typeof utils);
 
 /**
  * 必须要通过挂载在 window 对象上，才能够被传递到 electronic 中
